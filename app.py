2236095ce354ad41b46bea415d04c4d4a88edda7
SadineniAbhi <abhisadineni@gmail.com>
2023-04-28 13:16:41 +0000

    before cloud deployment

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask as fl
+from flask import Flask ,render_template
 
-app = fl(__name__)
+app = Flask(__name__)
 
+JOBS = [
+  {'id':1,
+  'title':"data analyst",
+  'location':"bengalure",
+  'salary': "10,00,000"
+  },
+  {'id':1,
+  'title':"data scientist",
+  'location':"dheli ",
+  'salary': "19,00,000"
+  },
+  {'id':1,
+  'title':"front end engineer",
+  'location':"remote"
+  },
+  {'id':1,
+  'title':"back end engineer",
+  'location':"bengalure",
+  'salary': "120,000"
+  }
+]
 @app.route("/")
 def hello_world():
-  return "hare krishna !"
+  return render_template ('home.html',jobs = JOBS,companyname = "jovian")
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
